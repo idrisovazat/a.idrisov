2cdb89017471487597d9b110f4d19dc099506ea3
idrisovazat <idrisovazat@list.ru>
2015-04-28 11:55:52 +0300

    upd

--- FFT/src/ru/kpfu/itis/group11408/fft/Complex.java
+++ FFT/src/ru/kpfu/itis/group11408/fft/Complex.java
@@ -6,53 +6,17 @@ package ru.kpfu.itis.group11408.fft;
 public class Complex {
     double re;
     double im;
-    double mas[][];
-
-    Complex(double[] a){
-        int n = a.length;
-        mas = new double[n][2];
-        for (int i = 0; i < n; i++) {
-            mas[i][0] = a[i];
-            mas[i][1] = 0;
-        }
-    }
 
     Complex(double re, double im){
         this.re = re;
         this.im = im;
     }
 
-    Complex(){
-    }
     public double getRe(){
         return re;
     }
-
     public double getIm(){
         return im;
-    }
-
-
-    public void w(int n, int k){
-        re = Math.cos(2*Math.PI*k/n);
-        im = Math.sin(2 * Math.PI * k / n);
-    }
-
-    public Complex plus(Complex a, Complex b){
-        return new Complex(a.getRe() + b.getRe(), a.getIm() + b.getIm());
-    }
-
-    public Complex plus(double re1, double im1, double re2, double im2){
-        return new Complex(re1 + re2, im1 + im2);
-    }
-
-    public void plus(double re, double im){
-        this.re += re;
-        this.im += im;
-    }
-    public void mult(double chislo){
-        this.re *= chislo;
-        this.im *= chislo;
     }
 
     public void mult(Complex b){
@@ -66,12 +30,10 @@ public class Complex {
         this.re += b.re;
         this.im += b.im;
     }
-
     public void minus(Complex b){
         this.re -= b.re;
         this.im -= b.im;
     }
-
     public void delay(double n) {
         this.re /= n;
         this.im /= n;
--- FFT/src/ru/kpfu/itis/group11408/fft/Main.java
+++ FFT/src/ru/kpfu/itis/group11408/fft/Main.java
@@ -1,23 +1,55 @@
 package ru.kpfu.itis.group11408.fft;
 
-/**
- * Created by Татагромодуль on 27.04.2015.
- */
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        int n = 8;
-        double[] a = new double[n];
-        double[] b = new double[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = Math.floor(Math.random()*10);
-            System.out.print(a[i] + "  a ");
-            b[i] = Math.floor(Math.random()*10);
-            System.out.println(b[i] + " b ");
-        }
-        FFT f = new FFT(a,b);
-        Complex[] mas = f.start();
-        for (int i = 0; i < mas.length; i++) {
-            System.out.println(mas[i].getRe() + " +i" + mas[i].getIm());
-        }
+        start(64);
+        System.out.println("-----------------------------");
+        start(8192);
+        System.out.println("-----------------------------");
+        start(8192 * 64);
+    }
+    public static void start(int n){
+        try {
+            double[] a = new double[n];
+            double[] b = new double[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = Math.floor(Math.random() * 10);
+                b[i] = Math.floor(Math.random() * 10);
+            }
+            long start = System.currentTimeMillis();
+            FFT f = new FFT(a, b);
+            Complex[] mas = f.start();
+            for (int i = 0; i < mas.length; i++) {
+// System.out.print(mas[i].getRe() + "+");
+            }
+            long end = System.currentTimeMillis();
+            System.out.println();
+            System.out.println("Время работы методом Фурье при n=" + n + ": " + countTime(start, end) + " секунд");
+
+            start = System.currentTimeMillis();
+            Object[] c = multiply(a, b);
+            end = System.currentTimeMillis();
+            System.out.println("Время работы простым методом при n=" + n + ": " + countTime(start, end) + " секунд");
+        }catch(Exception e){ e.printStackTrace();}
+    }
+    private static Object[] multiply(double[] list1, double[] list2) {
+        ArrayList<Double> array =new ArrayList<Double>(list1.length+list2.length);
+
+        for (int i = 0 ; i < list1.length + list2.length ; i++)
+            array.add((double) 0);
+        for (int i = 0; i < list1.length; i++)
+            for (int j = 0; j < list2.length; j++)
+                array.set(i+j, ((list1[i] * list2[j])+ array.get(i + j)));
+
+        return array.toArray();
+
+    }
+    public static double countTime(long start, long end){
+        long delta = end - start;
+        return delta/1000.0;
     }
 }
